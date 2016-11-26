c4f7e4bc47121aa649558b23f095d929f7964d6a
Puneeth Naik <puneethn250@gmail.com>
2016-11-26 20:16:42 +0530

    Again made Changes

--- app/src/main/java/com/hackathon/rsinha/smartbusses/MapsActivity.java
+++ app/src/main/java/com/hackathon/rsinha/smartbusses/MapsActivity.java
@@ -1,18 +1,13 @@
 package com.hackathon.rsinha.smartbusses;
 
-import android.*;
-import android.Manifest;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,21 +16,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPoiClickListener {
 
     private GoogleMap mMap;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +39,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        getLocalBusStation(12.933657,77.6123021);
     }
 
+    public void getLocalBusStation(double mLatitude,double mLongitude) {
+
+        String type="bus_station";
+        StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        sb.append("location=" + mLatitude + "," + mLongitude);
+        sb.append("&radius=5000");
+        sb.append("&types=" + type);
+        sb.append("&sensor=true");
+        sb.append("&key="+getResources().getString(R.string.google_maps_key));
+
+        Log.i("Check","Url : "+sb.toString());
+        // Creating a new non-ui thread task to download json data
+        PlacesTask placesTask = new PlacesTask();
+
+        // Invokes the "doInBackground()" method of the class PlaceTask
+        placesTask.execute(sb.toString());
+    }
 
     /**
      * Manipulates the map once available.
@@ -76,7 +85,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -93,52 +102,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onPoiClick(PointOfInterest poi) {
 
-        Log.i("Check", "Working");
-        Toast.makeText(getApplicationContext(), "Clicked: " +
-                        poi.name + "\nPlace ID:" + poi.placeId +
-                        "\nLatitude:" + poi.latLng.latitude +
-                        " Longitude:" + poi.latLng.longitude,
-                Toast.LENGTH_SHORT).show();
+            Log.i("Check", "Working");
+            Toast.makeText(getApplicationContext(), "Clicked: " +
+                            poi.name + "\nPlace ID:" + poi.placeId +
+                            "\nLatitude:" + poi.latLng.latitude +
+                            " Longitude:" + poi.latLng.longitude,
+                    Toast.LENGTH_SHORT).show();
     }
 
 
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
+//Temporary
+private String downloadUrl(String strUrl) throws IOException {
+    String data = "";
+    InputStream iStream = null;
+    HttpURLConnection urlConnection = null;
+    try {
+        URL url = new URL(strUrl);
 
-            // Creating an http connection to communicate with url
-            urlConnection = (HttpURLConnection) url.openConnection();
+        // Creating an http connection to communicate with url
+        urlConnection = (HttpURLConnection) url.openConnection();
 
-            // Connecting to url
-            urlConnection.connect();
-            // Reading data from url
-            iStream = urlConnection.getInputStream();
+        // Connecting to url
+        urlConnection.connect();
+        // Reading data from url
+        iStream = urlConnection.getInputStream();
 
-                        BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-             
-                        StringBuffer sb =new StringBuffer();
-             
-                        String line = "";
-                        while ((line = br.readLine()) !=null){
-                                sb.append(line);
-                            }
-             
-                        data = sb.toString();
-             
-                        br.close();
-             
-                    } catch (Exception e) {
-                        Log.d("Exception while downloading url", e.toString());
-                    } finally {
-                        iStream.close();
-                        urlConnection.disconnect();
-                    }
-         
-                return data;
-            }
+        BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+        StringBuffer sb = new StringBuffer();
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        data = sb.toString();
+        br.close();
+    } catch (Exception e) {
+        //Log.d("Exception while downloading url", e.toString());
+    } finally {
+        iStream.close();
+        urlConnection.disconnect();
+    }
+    return data;
+}
 
     @Override
     public void onStart() {
@@ -146,7 +151,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
+        /*client.connect();
         Action viewAction = Action.newAction(
                 Action.TYPE_VIEW, // TODO: choose an action type.
                 "Maps Page", // TODO: Define a title for the content shown.
@@ -158,6 +163,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Uri.parse("android-app://com.hackathon.rsinha.smartbusses/http/host/path")
         );
         AppIndex.AppIndexApi.start(client, viewAction);
+        */
     }
 
     @Override
@@ -166,7 +172,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
+        /*Action viewAction = Action.newAction(
                 Action.TYPE_VIEW, // TODO: choose an action type.
                 "Maps Page", // TODO: Define a title for the content shown.
                 // TODO: If you have web page content that matches this app activity's content,
@@ -178,126 +184,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         );
         AppIndex.AppIndexApi.end(client, viewAction);
         client.disconnect();
+        */
     }
 
-     
-                /** A class, to download Google Places */
-                
+    /**
+     * A class, to download Google Places
+     */
 
     private class PlacesTask extends AsyncTask<String, Integer, String> {
-         
-                        
+
         String data = null;
-         
-                        // Invoked by execute() method of this object
-                        
-        @Override
-                
+// Invoked by execute() method of this object
 
+        @Override
         protected String doInBackground(String... url) {
-                        try {
-                                data = downloadUrl(url[0]);
-                            } catch (Exception e) {
-                                Log.d("Background Task", e.toString());
-                            }
-                        return data;
-                    }
+            Log.i("Check","Fetching Data");
+            try {
+                data = downloadUrl(url[0]);
+                Log.i("Check","Done Fetching Data");
+            } catch (Exception e) {
+                Log.d("Check", e.toString());
+            }
+            return data;
+        }
+// Executed after the complete execution of doInBackground() method
 
-         
-                        // Executed after the complete execution of doInBackground() method
-                        
         @Override
-                
-
         protected void onPostExecute(String result) {
-                        ParserTask parserTask = new ParserTask();
-             
-                        // Start parsing the Google places in JSON format
-                        // Invokes the "doInBackground()" method of the class ParseTask
-                        parserTask.execute(result);
-                    }
+            Log.i("Check","Starting to parse Data");
+            ParserTask parserTask = new ParserTask();
 
-         
-                    
+            // Start parsing the Google places in JSON format
+            // Invokes the "doInBackground()" method of the class ParseTask
+            parserTask.execute(result);
+        }
+
+
     }
 
-     
-                /** A class to parse the Google Places in JSON format */
-                
+
+    /**
+     * A class to parse the Google Places in JSON format
+     */
 
     private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String, String>>> {
-         
-                        
         JSONObject jObject;
-         
-                        // Invoked by execute() method of this object
-                        
-        @Override
-                
+// Invoked by execute() method of this object
 
+        @Override
         protected List<HashMap<String, String>> doInBackground(String... jsonData) {
-             
-                        List<HashMap<String, String>> places = null;
-                        PlaceJSONParser placeJsonParser = new PlaceJSONParser();
-             
-                        try {
-                                jObject = new JSONObject(jsonData[0]);
-                 
-                                /** Getting the parsed data as a List construct */
-                                places = placeJsonParser.parse(jObject);
-                 
-                            } catch (Exception e) {
-                                Log.d("Exception", e.toString());
-                            }
-                        return places;
-                    }
 
-         
-                        // Executed after the complete execution of doInBackground() method
-                        
+            List<HashMap<String, String>> places = null;
+            PlaceJSONParser placeJsonParser = new PlaceJSONParser();
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+
+                /** Getting the parsed data as a List construct */
+                places = placeJsonParser.parse(jObject);
+
+            } catch (Exception e) {
+                Log.d("Exception", e.toString());
+            }
+            return places;
+        }
+
+
+// Executed after the complete execution of doInBackground() method
+
         @Override
-                
-
         protected void onPostExecute(List<HashMap<String, String>> list) {
-             
-                        // Clears all the existing markers
-                        mGoogleMap.clear();
-             
-                        for (int i = 0; i < list.size(); i++) {
-                 
-                                // Creating a marker
-                                MarkerOptions markerOptions = new MarkerOptions();
-                 
-                                // Getting a place from the places list
-                                HashMap<String, String> hmPlace = list.get(i);
-                 
-                                // Getting latitude of the place
-                                double lat = Double.parseDouble(hmPlace.get("lat"));
-                 
-                                // Getting longitude of the place
-                                double lng = Double.parseDouble(hmPlace.get("lng"));
-                 
-                                // Getting name
-                                String name = hmPlace.get("place_name");
-                 
-                                // Getting vicinity
-                                String vicinity = hmPlace.get("vicinity");
-                 
-                                LatLng latLng = new LatLng(lat, lng);
-                 
-                                // Setting the position for the marker
-                                markerOptions.position(latLng);
-                 
-                                // Setting the title for the marker.
-                                //This will be displayed on taping the marker
-                                markerOptions.title(name + " : " + vicinity);
-                 
-                                // Placing a marker on the touched position
-                                mGoogleMap.addMarker(markerOptions);
-                            }
-                    }
 
-            
+            // Clears all the existing markers
+            mMap.clear();
+            Log.i("Check","Got the data parsed Total List Size : "+list.size());
+            for (int i = 0; i < list.size(); i++) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Getting a place from the places list
+                HashMap<String, String> hmPlace = list.get(i);
+
+                // Getting latitude of the place
+                double lat = Double.parseDouble(hmPlace.get("lat"));
+
+                // Getting longitude of the place
+                double lng = Double.parseDouble(hmPlace.get("lng"));
+
+                // Getting name
+                String name = hmPlace.get("place_name");
+
+                // Getting vicinity
+                String vicinity = hmPlace.get("vicinity");
+
+                LatLng latLng = new LatLng(lat, lng);
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                //This will be displayed on taping the marker
+                markerOptions.title(name + " : " + vicinity);
+
+                // Placing a marker on the touched position
+                mMap.addMarker(markerOptions);
+            }
+        }
+
     }
-
-     
+}
